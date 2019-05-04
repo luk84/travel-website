65a5fe5caab2ae0bd2de293ce11b6860e4775f6d
Lukasz Bukowski <luk3@interia.pl>
2019-05-04 21:19:51 +0200

    Added browser support fallbacks (but no svg2png, no flex, no medornizer tool)

--- app/assets/scripts/Vendor.js
+++ app/assets/scripts/Vendor.js
@@ -1,2 +1,3 @@
 // packages like lazysizes run on their own without any additional code so good practice to place it in a separate file. Then update webpack config so can build a final script file
 import "lazysizes";
+import "picturefill";
